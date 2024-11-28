eb9d33a9d5717d2063c82a0aa657c8f9a1b6135a
luisreyjor12@gmail.com <luisreyjor12@gmail.com>
2024-11-28 13:29:46 -0500

    a

---
+++ src/main/java/com/example/demo/EjemploGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploGithubApplication.class, args);
+	}
+
+}
